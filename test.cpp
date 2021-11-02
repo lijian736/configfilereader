bc9931cb1837db695980ac7933222fd94ff6cc1d
videoaudiolijian <videoaudio_nanjing@163.com>
2021-11-02 09:17:50 +0800

    initial commit

---
+++ test.cpp
@@ -0,0 +1,26 @@
+#include "app_config.h"
+#include <iostream>
+
+int main()
+{
+	AppConfig* config = AppConfig::get_instance();
+	if (config->initialize("config.cfg", "=", "#"))
+	{
+		bool ret = config->is_config_file_exists();
+		std::cout << "The config file [config.cfg] exists:" << ret << std::endl;
+
+		ret = config->read_and_parse();
+		std::cout << "The config file [config.cfg] read and parse:" << ret << std::endl;
+
+		std::string serverIP = AppConfig::get_instance()->get_by_key("server_ip", "127.0.0.1");
+		std::string serverPort = AppConfig::get_instance()->get_by_key("server_port", "8080");
+		std::cout << "server ip: " << serverIP << " port: " << serverPort << std::endl;
+
+		std::cout << "the configs list: \n" << *config << std::endl;
+	}
+
+	system("pause");
+
+    return 0;
+}
+
